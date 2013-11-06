7ba6d82955418b2d742379549fb77f6362cbb34e
Lindsey Bieda <hazardous.narayan@gmail.com>
2013-11-06 18:44:47 -0500

    Get really basic animation working.

---
+++ animated_sprite.lua
@@ -0,0 +1,43 @@
+AnimatedSprite = {}
+AnimatedSprite.__index = AnimatedSprite
+
+function AnimatedSprite:create(file, width, height, frames)
+	local object = {}
+
+	setmetatable(object, AnimatedSprite)
+
+	object.file = file
+	object.width = width
+	object.height = height
+	object.frames = frames
+	object.sprite_sheet = sprite_sheet
+	object.sprites = {}
+	object.current_frame = 1
+	object.delay = 0.1
+	object.delta = 0
+
+	return object
+end
+
+function AnimatedSprite:load()
+	local source = love.graphics.newImage(self.file)
+	self.sprite_sheet = love.graphics.newImage(self.file)
+
+	for i = 1, self.frames do
+		local w = self.width * (i-1)
+		self.sprites[i] = love.graphics.newQuad(w, self.height, self.width, self.height, source:getWidth(), source:getHeight())
+	end
+end
+
+function AnimatedSprite:update(dt)
+	self.delta = self.delta + dt
+
+	if self.delta >= self.delay then
+		self.current_frame = (self.current_frame % self.frames) + 1
+		self.delta = 0
+	end
+end
+
+function AnimatedSprite:draw(x, y)
+	love.graphics.drawq(self.sprite_sheet, self.sprites[self.current_frame], x, y)
+end
--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
+require "animated_sprite"
+
 function love.load()
 	hero = {}
 	hero.x = 300
 	hero.y = 400
 	hero.speed = 300
+	-- hero_sprite = love.graphics.newImage("sprites/mouse.png")
+	animation = AnimatedSprite:create("sprites/hero_sprites.png", 64, 128, 10)
+	animation:load()
 end
 
 function love.update(dt)
+	animation:update(dt)
+
 	if love.keyboard.isDown("left") then
 		hero.x = hero.x - hero.speed * dt
 	elseif love.keyboard.isDown("right") then
@@ -19,6 +26,7 @@ function love.draw()
 	love.graphics.setColor(0,255,0,255)
 	love.graphics.rectangle("fill", 0, 465, 800, 150)
 
-	love.graphics.setColor(255,255,0,255)
-	love.graphics.rectangle("fill",hero.x,hero.y, 30, 65)
+	love.graphics.setColor(255,255,255,255)
+	animation:draw(hero.x, hero.y)
+	-- love.graphics.draw(hero_sprite, hero.x, hero.y)
 end
