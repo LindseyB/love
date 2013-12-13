6de25c4821a246d40f3cf69c5471cce2c27b6489
Lindsey Bieda <hazardous.narayan@gmail.com>
2013-12-12 20:07:04 -0500

    Create the hero object.

---
+++ hero.lua
@@ -0,0 +1,70 @@
+require "animated_sprite"
+
+Hero = {}
+Hero.__index = Hero
+
+function Hero:create()
+	local object = {}
+
+	setmetatable(object, Hero)
+
+	object.x = 300
+	object.y = 400
+	object.speed = 300
+	object.animation = AnimatedSprite:create("sprites/hero.png", 32, 32, 4, 4)
+
+	object.Directions = {
+		["Down"] = 1,
+		["Left"] = 2,
+		["Right"] = 3,
+		["Up"] = 4
+	}
+
+	return object
+end
+
+function Hero:load()
+	self.animation:load()
+end
+
+function Hero:move(direction, dt)
+	if direction == self.Directions.Down then
+		self.y = self.y + self.speed * dt
+		self.animation:set_animation_direction(self.animation.Directions.Down)
+	end
+
+	if direction == self.Directions.Left then
+		self.x = self.x - self.speed * dt
+		self.animation:set_animation_direction(self.animation.Directions.Left)
+	end
+
+	if direction == self.Directions.Right then
+		self.x = self.x + self.speed * dt
+		self.animation:set_animation_direction(self.animation.Directions.Right)
+	end
+
+	if direction == self.Directions.Up then
+		self.y = self.y - self.speed * dt
+		self.animation:set_animation_direction(self.animation.Directions.Up)
+	end
+
+	if hero.x > love.graphics.getWidth()-32 then hero.x = love.graphics.getWidth()-32 end
+	if hero.x < 0 then hero.x = 0 end
+
+	if hero.y > love.graphics.getHeight()-32 then hero.y = love.graphics.getHeight()-32 end
+	if hero.y < 0 then hero.y = 0 end
+end
+
+function Hero:stop()
+	self.animation:set_animation(false)
+end
+
+function Hero:draw()
+	self.animation:draw(self.x, self.y)
+end
+
+function Hero:update(dt)
+	if(self.animaton) then
+		self.animation:update(dt)
+	end
+end
--- main.lua
+++ main.lua
@@ -1,44 +1,30 @@
-require "animated_sprite"
+require "hero"
 
 function love.load()
-	hero = {}
-	hero.x = 300
-	hero.y = 400
-	hero.speed = 300
-	animation = AnimatedSprite:create("sprites/hero.png", 32, 32, 4, 4)
-	animation:load()
+	hero = Hero.create()
+	hero:load()
 end
 
 function love.update(dt)
-	animation:update(dt)
+	Hero:update(dt)
 
 	-- todo: move all the hero movement to a different file to be called in this loop
 	if love.keyboard.isDown("left") then
-		hero.x = hero.x - hero.speed * dt
-		animation:set_animation_direction(animation.Directions.Left)
+		hero:move(hero.Directions.Left, dt)
 	elseif love.keyboard.isDown("right") then
-		hero.x = hero.x + hero.speed*dt
-		animation:set_animation_direction(animation.Directions.Right)
+		hero:move(hero.Directions.Right, dt)
 	elseif love.keyboard.isDown("up") then
-		hero.y = hero.y - hero.speed*dt
-		animation:set_animation_direction(animation.Directions.Up)
+		hero:move(hero.Directions.Up, dt)
 	elseif love.keyboard.isDown("down") then
-		hero.y = hero.y + hero.speed*dt
-		animation:set_animation_direction(animation.Directions.Down)
+		hero:move(hero.Directions.Down, dt)
 	elseif love.keyboard.isDown("q") then
 		love.event.push('quit')
 	else
-		animation:set_animation(false)
+		hero:stop()
 	end
-
-	if hero.x > love.graphics.getWidth()-32 then hero.x = love.graphics.getWidth()-32 end
-	if hero.x < 0 then hero.x = 0 end
-
-	if hero.y > love.graphics.getHeight()-32 then hero.y = love.graphics.getHeight()-32 end
-	if hero.y < 0 then hero.y = 0 end
 end
 
 function love.draw()
 	love.graphics.setColor(255,255,255,255)
-	animation:draw(hero.x, hero.y)
+	hero:draw()
 end
