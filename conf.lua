a60479009b27be6220879a269b34ca6aea6eef0c
Lindsey Bieda <lbieda@articulate.com>
2014-01-31 15:38:04 -0500

    Removing some unused modules

--- conf.lua
+++ conf.lua
@@ -1,6 +1,8 @@
 function love.conf(t)
 	t.title = "Game Thing"
 	t.version = "0.9.0"
+    t.modules.joystick = false
+    t.modules.physics = false
 	-- fullscreen the thing
 	-- t.window.fullscreen = true
 end
